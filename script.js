bcc2d1b63953e0d72a102d28cb51bc5082c5b220
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-08-31 13:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,79 @@
-//complete this code
-class Person {}
+// Person class
+class Person {
+  /**
+   * Constructor method
+   * @param {string} name - The name of the person
+   * @param {number} age - The age of the person
+   */
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  /**
+   * Getter method for name
+   * @returns {string} The name of the person
+   */
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  /**
+   * Setter method for age
+   * @param {number} age - The new age of the person
+   */
+  set age(age) {
+    this._age = age;
+  }
+}
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+// Student class
+class Student extends Person {
+  /**
+   * Constructor method
+   * @param {string} name - The name of the student
+   * @param {number} age - The age of the student
+   */
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  /**
+   * Method to log that the student is studying
+   */
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Teacher class
+class Teacher extends Person {
+  /**
+   * Constructor method
+   * @param {string} name - The name of the teacher
+   * @param {number} age - The age of the teacher
+   */
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  /**
+   * Method to log that the teacher is teaching
+   */
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// Example usage
+const person = new Person("John", 25);
+console.log(person.name); // Output: John
+
+person.age = 30;
+console.log(person.age); // Output: 30
+
+const student = new Student("Alice", 22);
+student.study(); // Output: Alice is studying
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); // Output: Bob is teaching
